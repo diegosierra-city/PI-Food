b323c8f0a76bfb85a9bc471c55979bf0f23c642f
diegosierra <diegosierra@cityciudad.com>
2023-08-15 23:42:42 -0500

    ajuste en la ubicación de la carpeta de imagenes

--- api/src/controllers/getAllRecipes.js
+++ api/src/controllers/getAllRecipes.js
@@ -23,16 +23,16 @@ recipesDB = await Recipe.findAll({
   ],
 })
 
-/*   const response = await axios.get(
+  const response = await axios.get(
    `${API_URL}/complexSearch?number=100&addRecipeInformation=true&apiKey=${API_KEY}`
  );
-recipesAPI = response.data.results; */
+recipesAPI = response.data.results;
+return res.status(200).json([...recipesDB,...recipesAPI]);
 
+/* recipesAPI = db.results
+return res.status(200).json([...recipesDB]);
+ */
 
-recipesAPI = db.results
-
-  //return res.status(200).json([...recipesDB]);
-  return res.status(200).json([...recipesDB,...recipesAPI]);
  } catch (error) {
    return res.status(404).json(error.message);
  }
